b45a07167baf7b9948feb91bdb8a7be991f629e4
David Zhao <david@zecter.com>
2009-07-10 19:33:51 -0700

    Added funnel tracking

--- lib/mixpanel/event_logger.rb
+++ lib/mixpanel/event_logger.rb
@@ -18,21 +18,11 @@ module Mixpanel
     end
     
     def record( name, props = {}, request = nil )
-      uri = URI.parse( generate_url(name, props, {}, request) )
-      req = Net::HTTP::Post.new(uri.path)
-      req.body = uri.query
-      res = Net::HTTP.start(uri.host, uri.port) {|http|
-        http.request(req)
-      }
-      
-      if( !res.is_a?(Net::HTTPSuccess) )
-        error = "Failed to log event:#{name}, properties:#{props.inspect}"
-        if( @logger )
-          @logger.error( error )
-        else
-          STDERR.puts( error )
-        end
-      end
+      send_request( generate_url(name, props, {}, request) )
+    end
+    
+    def record_funnel( funnel, step, goal )
+      send_request( generate_funnel_url(funnel, step, goal) )
     end
     
     # details of which params are allowed, see:
@@ -48,7 +38,7 @@ module Mixpanel
         event_props[:ip] = request.remote_ip
       end
       
-      data = { :event => 'name', :properties => event_props }
+      data = { :event => name, :properties => event_props }
       
       encoded = Base64::encode64(data.to_json).gsub(/\s/, '')
       params[:data] = encoded
@@ -58,6 +48,34 @@ module Mixpanel
       }
       url = "#{@@endpoint}/track/?#{param_strings.join('&')}"
       return url
+    end
+    
+    def generate_funnel_url( funnel, step, goal, props = {}, params = {}, request = nil )
+      return generate_url( 'mp_funnel',
+        props.merge( :funnel => funnel, :step => step, :goal => goal ),
+        params, request )
+    end
+    
+    protected
+    
+    def send_request( url )
+      uri = URI.parse( url )
+      req = Net::HTTP::Post.new(uri.path)
+      req.body = uri.query
+      res = Net::HTTP.start(uri.host, uri.port) {|http|
+        http.request(req)
+      }
+      
+      if( !res.is_a?(Net::HTTPSuccess) )
+        error = "Failed to log event:#{name}, properties:#{props.inspect}"
+        if( @logger )
+          @logger.error( error )
+        else
+          STDERR.puts( error )
+        end
+        return false
+      end
+      return true
     end
   end
 end
